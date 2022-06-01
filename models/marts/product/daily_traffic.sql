fa5788ec0a20879224a4b10b98e192308c66faa7
Greg Clunies <greg.clunies@gmail.com>
2022-06-01 08:34:37 -0700

    tests: for daily_traffic

--- models/marts/product/daily_traffic.sql
+++ models/marts/product/daily_traffic.sql
@@ -16,7 +16,7 @@ final as (
 
     select
         tstamp::date as date_day,
-        count(distinct coalesce(user_id, anonymous_id)) as visitors
+        count(distinct coalesce(user_id, anonymous_id)) as count_visitors
 
     from pages
     group by 1
