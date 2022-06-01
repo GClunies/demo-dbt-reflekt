9e83b1a10344a67fa794f07420a750d60a93ad93
Greg Clunies <greg.clunies@gmail.com>
2022-06-01 08:30:48 -0700

    feat: daily_traffic model & docs

---
+++ models/marts/product/daily_traffic.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    materialized = 'view',
+    )
+}}
+
+with
+
+pages as (
+
+    select * from {{ ref('reflekt_patty_bar_web__pages') }}
+
+),
+
+final as (
+
+    select
+        tstamp::date as date_day,
+        count(distinct coalesce(user_id, anonymous_id)) as visitors
+
+    from pages
+    group by 1
+
+)
+
+select * from final
